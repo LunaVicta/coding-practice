8ae6a63d3709ffb4cfb9da0622b0d06cc174ba57
LunaVicta <monstersmakemehorny@gmail.com>
2021-03-26 12:40:41 -0400

    changed 8,7,10

--- Project_Euler_10.py
+++ Project_Euler_10.py
@@ -1,12 +1,13 @@
 primes = [2]
 i = 3
-while(len(primes)<10001):
+while(primes[-1]<2000000):
     e = 0
     b = True
     while(e<len(primes) and b):
         b = not (i%primes[e]==0)
         e += 1
     if(b):
+        print(i)
         primes.append(i)
     i += 2
-print(primes[10000])
+print(sum(primes))
