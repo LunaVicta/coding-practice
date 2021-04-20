64e658596a78b9cba2715bff190d9d7ffe79b376
LunaVicta <monstersmakemehorny@gmail.com>
2021-04-20 14:22:23 -0400

    Basically finished a couple Ruby things.

--- ruby-practice/grep.rb
+++ ruby-practice/grep.rb
@@ -1,4 +1,19 @@
+puts "Enter a file name: "
 fileName = gets
+puts fileName
+puts fileName.class
+fileName = 'dogs.txt'
+puts
+puts "Enter a search string: "
+searchString = gets
 File.open(fileName, 'r') do |f|
-    
+    line = 0
+    f.each do
+        |l|
+        if l.include? searchString
+            puts l
+            puts line
+        end
+        line += 1
+    end
 end
--- ruby-practice/tree.rb
+++ ruby-practice/tree.rb
@@ -1,8 +1,12 @@
 class Tree
     attr_accessor :children, :node_name
-    def initialize(name, children=[])
-        @children = children
-        @node_name = name
+    def initialize(hashmap)
+        @children = []
+        hashmap.each_pair do
+            |key, value|
+            @node_name = key
+            value.each_pair { |k,v| @children.append(Tree.new({k => v}))}
+        end
     end
 
     def visit_all(&block)
@@ -14,11 +18,5 @@ class Tree
         block.call self
     end
 end
-
-ruby_tree = Tree.new("Ruby", [Tree.new("Reia"), Tree.new("MacRuby")])
-puts "Visiting a node."
-ruby_tree.visit {|node| puts node.node_name}
-puts
-
-puts "Visiting entire tree."
-ruby_tree.visit_all {|node| puts node.node_name}
+family = Tree.new({"grandpa"=>{"dad"=>{"child 1"=>{}, "child 2"=>{}}, "uncle"=>{"child 3"=>{}, "child 4"=>{}}}})
+family.visit_all {|node| puts node.node_name}
