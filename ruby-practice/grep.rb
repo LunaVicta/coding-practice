8c743c1693984c884f8110708905d32243c1ebcc
LunaVicta <monstersmakemehorny@gmail.com>
2021-04-20 14:41:37 -0400

    Figured out the bloody gets method

--- ruby-practice/grep.rb
+++ ruby-practice/grep.rb
@@ -1,12 +1,9 @@
-#this basically works, but I don't understand the gets method so it can't read in the file name correctly
 puts "Enter a file name: "
-fileName = gets
-puts fileName
-puts fileName.class
-fileName = 'dogs.txt'
-puts
+fileName = gets.chomp
 puts "Enter a search string: "
-searchString = gets
+searchString = gets.chomp
+puts
+puts
 File.open(fileName, 'r') do |f|
     line = 0
     f.each do
