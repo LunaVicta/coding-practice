9d50a9e51b18a544f09218706d64358019879a74
LunaVicta <monstersmakemehorny@gmail.com>
2021-04-26 10:08:03 -0400

    Well the mv didn't work so I cheated.

--- ruby-practice/acts_as_csv_module.rb
+++ ruby-practice/acts_as_csv_module.rb
@@ -1,20 +1,38 @@
 module ActsAsCsv
-    
     def self.included(base)
-        base.extend ClassMethods
+      base.extend ClassMethods
     end
-
+    
     module ClassMethods
-        def acts_as_csv
-            include IstanceMethods
-        end
+      def acts_as_csv
+        include InstanceMethods
+      end
     end
-
-    module InstanceMethods
-        
-        def read
-            @csv_contents = []
-            filename = self.class.to_s.downcase + '.txt'
+    
+    module InstanceMethods   
+      def read
+        @csv_contents = []
+        filename = self.class.to_s.downcase + '.txt'
+        file = File.new(filename)
+        @headers = file.gets.chomp.split(', ')
+  
+        file.each do |row|
+          @csv_contents << row.chomp.split(', ')
         end
+      end
+      
+      attr_accessor :headers, :csv_contents
+      def initialize
+        read 
+      end
     end
-end
+  end
+  
+  class RubyCsv  # no inheritance! You can mix it in
+    include ActsAsCsv
+    acts_as_csv
+  end
+  
+  m = RubyCsv.new
+  puts m.headers.inspect
+  puts m.csv_contents.inspect
