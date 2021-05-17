23caf464082e36b31daf010dedd8e3d2b091b0e0
LunaVicta <monstersmakemehorny@gmail.com>
2021-05-17 09:01:27 -0400

    Can't remember what I did. Bad Luna. Probably futzed with the PE11?

--- python-practice/Project_Euler_11.py
+++ python-practice/Project_Euler_11.py
@@ -26,24 +26,29 @@ with open("number_grid.txt") as f:
 max_number = 1
 row_number = 0
 counter = 0
+location = [0,0]
 while(row_number < len(grid)):
     col_number = 0
     while(col_number < len(grid)):
-        print(grid[row_number][col_number], end=" ")
         if(row_number+3 < len(grid) and col_number+3 < len(grid[0])):
             rp = row_product(grid, row_number, col_number, 4)
             cp = col_product(grid, row_number, col_number, 4)
             dp = diag_product(grid, row_number, col_number, 4)
+            prev_max = max_number
             max_number = max(max_number, rp, cp, dp)
-            counter += 3
+            if(prev_max != max_number):
+                location = [col_number,row_number]
         elif(row_number+3 < len(grid)):
+            prev_max = max_number
             max_number = max(max_number, col_product(grid, row_number, col_number, 4))
-            counter += 1
+            if(prev_max != max_number):
+                location = [col_number,row_number]
         elif(col_number+3 < len(grid[0])):
+            prev_max = max_number
             max_number = max(max_number, row_product(grid, row_number, col_number, 4))
-            counter += 1
+            if(prev_max != max_number):
+                location = [col_number,row_number]
         col_number += 1
-    print()
     row_number += 1
 print(max_number)
-print(counter)
+print(location)
